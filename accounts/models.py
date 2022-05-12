f9d692b0e5c32298ebe2dff489de2af35580fa59
Priyamesh Gupta <priyamesh28@gmai.com>
2022-05-12 11:51:23 +0530

    model setup

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
@@ -11,6 +12,12 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+class Tag(models.Model):
+    name=models.CharField(max_length=25,null=True)
+
+    def __str__(self):
+        return self.name
     
 class Product(models.Model):
     CATEGORY = (
@@ -20,9 +27,12 @@ class Product(models.Model):
     name=models.CharField(max_length=25, null=True)
     price=models.FloatField(null=True)
     catageory=models.CharField(max_length=25, null=True,choices=CATEGORY)
-    description=models.CharField(max_length=100, null=True)
+    description=models.CharField(max_length=100, null=True,blank=True)
     date_created=models.DateTimeField(auto_now_add=True,null=True)
+    tag=models.ManyToManyField(Tag)
 
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     STATUS = (
@@ -30,8 +40,8 @@ class Order(models.Model):
 			('Out for delivery', 'Out for delivery'),
 			('Delivered', 'Delivered'),
 			)
-
     product=models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
     customer=models.ForeignKey(Customer,null=True,on_delete=models.SET_NULL)
     date_created=models.DateTimeField(auto_now_add=True,null=True)
     status=models.CharField(max_length=25,null=True,choices=STATUS)
+    
