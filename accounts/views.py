658e591d385dbd2ef3aea7700cab7a5f12aa6f61
Priyamesh Gupta <priyamesh28@gmai.com>
2022-05-16 18:59:39 +0530

    curd opeerations

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
+from accounts.forms import OrderForm
 from .models import *
 
 # Create your views here.
@@ -42,3 +43,42 @@ def customer(request,pk_test):
         'total_orders_count':total_orders_count,
     }
     return render(request,'accounts/customer.html',context)
+
+def createOrder(request):
+
+    form=OrderForm()
+    if request.method =='POST':
+        #print('printing request:',request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context={'form':form}
+    return render(request,'accounts/order_form.html',context)
+
+def updateOrder(request,pk):
+    order=Order.objects.get(id=pk)
+    form=OrderForm(instance=order)
+
+    if request.method =='POST':
+        #print('printing request:',request.POST)
+        form = OrderForm(request.POST,instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/') 
+    
+    context={'form':form}
+    return render(request,'accounts/order_form.html',context)
+
+
+def deleteOrder(request,pk):
+    order=Order.objects.get(id=pk)
+    context={'item':order}
+
+    if(request.method=="POST"):
+        order.delete()
+        return redirect('/') 
+        
+
+    return render(request,'accounts/delete_order.html',context)
